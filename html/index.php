6ecdde6339ed40a37d21e8fa2cc6edd925d05061
Yantama <tamaplace@icloud.com>
2025-08-05 00:06:44 +0900

    modified index.php

--- html/index.php
+++ html/index.php
@@ -6,20 +6,15 @@
 </head>
 <body>
 	<h1>Hello world!</h1>
-	<p><a style="display:none;" href="./pages/ayu.php">あゆ</a></p>
-	<p><a style="" href="./pages/ura.php">２次</a></p>
+	<p><a style="" href="./pages/ura.php">ura</a></p>
 	<p><a style="" href="./sql/reset.php">リセット</a></p>
-
+/*
     <form action="./script/submit.php" method="post" enctype="multipart/form-data">
     	<label>画像：<input type="file" name="picture"></label>
     	<input type="submit" value="送信">
     </form>
+*/
 
 	<hr>
-
-<?php
-phpinfo();
-?>
-
 </body>
 </html>
